b1926488d7973c6f9c839d70a6a7ff24ba99f460
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-05-21 14:37:20 +0200

    commit

--- src/main/java/com/example/demo/EurekaServerApplication.java
+++ src/main/java/com/example/demo/EurekaServerApplication.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
